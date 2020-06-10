6f0b0443b96fd9886b7e5379bb2e246c2ab2ca94
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-10 13:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function mondayWork(activity = "go to the office"){
   return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective(){
+function wrapAdjective(visualFlair = "*"){
   return function(adjective = "special"){
-    return `"You are ..."`
+    return `"You are ${adjective}"`
   }
 }
